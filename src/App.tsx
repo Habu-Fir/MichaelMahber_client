2e2a949f2f14767f0334df5af980d90115ef1ab3
unknown <tohabtish16@gmail.com>
2026-03-18 19:27:29 +0300

    App.ts Loan Detail page fiexd

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import ProtectedRoute from './components/auth/ProtectedRoute';
 import DashboardRouter from './components/dashboard/DashboardRouter';
 import Members from './pages/Members';
 import LoansPage from './pages/loans/LoansPage';
-import LoanDetailsPage from './pages/loans/LoanDetailsPageBkUp';
+import LoanDetailsPage from './pages/loans/LoanDetailsPage';
 import LoanRequestPage from './pages/loans/LoanRequestPage';
 import PaymentPage from './pages/loans/PaymentPage';
 import PendingPaymentsPage from './pages/loans/PendingPaymentsPage';
