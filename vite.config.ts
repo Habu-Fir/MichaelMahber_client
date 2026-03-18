50affcc010df7ba0b203dd8e87e6df65b3a5c951
unknown <tohabtish16@gmail.com>
2026-03-18 17:16:11 +0300

    react-is, rechart installation and vite configuration update

--- vite.config.ts
+++ vite.config.ts
@@ -30,6 +30,11 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      external: ['react-is'], // Add this line
+    },
+  },
   server: {
     port: 5173,
     open: true,
