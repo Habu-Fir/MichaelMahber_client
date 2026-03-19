347d66e925607ad72860b8ec8fe1d40b7ee08760
unknown <tohabtish16@gmail.com>
2026-03-19 14:25:43 +0300

    Modify Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,34 @@
+// // import { defineConfig } from 'vite'
+// // import react from '@vitejs/plugin-react'
+// // import tailwindcss from '@tailwindcss/vite'
+
+// // // https://vite.dev/config/
+// // export default defineConfig({
+
+// //   plugins: [react(), tailwindcss()],
+// //   server: {
+// //     port: 5173,
+// //     open: true,
+// //     proxy: {
+// //       '/api': {
+// //         target: 'https://michaelmahberbackend.onrender.com',
+// //         changeOrigin: true,
+// //       },
+// //     },
+// //   },
+// // })
+
+// // vite.config.js
+
+
+
+
+
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 // import tailwindcss from '@tailwindcss/vite'
 
-// // https://vite.dev/config/
 // export default defineConfig({
-
 //   plugins: [react(), tailwindcss()],
 //   server: {
 //     port: 5173,
@@ -13,26 +37,22 @@
 //       '/api': {
 //         target: 'https://michaelmahberbackend.onrender.com',
 //         changeOrigin: true,
+//         secure: false,
 //       },
 //     },
 //   },
 // })
-
-// vite.config.js
-
-
-
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist', // Make sure this is set
+  },
   server: {
     port: 5173,
-    open: true,
     proxy: {
       '/api': {
         target: 'https://michaelmahberbackend.onrender.com',
